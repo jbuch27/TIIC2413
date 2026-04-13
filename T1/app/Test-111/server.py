4573cc7344c8b6631895099a6684a7edb69703e4
{Matias Salinas} <{msalindd@gmail.com}>
2026-04-13 19:41:11 -0400

    appweb

--- T1/app/Test-111/server.py
+++ T1/app/Test-111/server.py
@@ -27,15 +27,31 @@ def index():
 
 @app.route('/search')
 def datos():
-    name = request.args.get('player')
-    conn = get_db_connection()
-    if conn is None:
-        return "Error de conexión a la base de datos", 500
+    data =  request.args.get('data')
+    if data == None:
+        return render_template('search.html', lista_datos=None)
     
+    conn = get_db_connection()
     with conn:
         with conn.cursor() as cur:
-            cur.execute('SELECT * FROM test;')
+            cur.execute('''
+                        SELECT DISTINCT E.nombre, E.fecha_creacion, E.capitan, ET.nombre_torneo, ET.fecha_inicio_torneo
+                        FROM EQUIPO E JOIN ESTA_EN_TORNEO ET ON E.nombre = ET.nombre_equipo
+                        WHERE E.nombre = %s;''',(data,))
             datos_db = cur.fetchall()
+            if datos_db ==[]:
+    
+                cur.execute('''
+                            SELECT DISTINCT J.gamertag , J.nombre, E.nombre AS nombre_equipo, 
+                                CASE
+                                    WHEN J.gamertag = E.capitan THEN 1
+                                    ELSE 0
+                                END AS es_capitan, J.fecha_nacimiento, J.email, J.pais_origen
+                            FROM JUGADOR J NATURAL JOIN ES_DEL_EQUIPO JE, EQUIPO E
+                            WHERE JE.NOMBRE_EQUIPO = E.NOMBRE AND (J.gamertag = %s OR J.pais_origen = %s);''',(data,data))
+                datos_db = cur.fetchall()
+            if datos_db == []:
+                datos_db = "DATA NOT FOUND"
     
     conn.close() 
     return render_template('search.html', lista_datos=datos_db)
@@ -54,20 +70,94 @@ def torneos():
     conn.close() 
     return render_template('torneo.html', lista_datos=datos_db)
 
+@app.route('/torneos/<string:id_torneo>')
+def torneo(id_torneo):
+    conn = get_db_connection()
+    with conn.cursor() as cur:
+        cur.execute('''
+                    SELECT P."id", P.nombre_equipo1, P.puntaje_equipo1, P.nombre_equipo2, P.puntaje_equipo2, P.inicio
+                    FROM Partida AS P 
+                    WHERE P.nombre_torneo = %s; ''', (id_torneo,))
+        datos_partidas = cur.fetchall()
+        cur.execute('''
+                    SELECT E.nombre AS Equipos_Inscritos, E.capitan AS Capitan, E.fecha_creacion
+                    FROM Esta_en_torneo AS ET
+                    JOIN Equipo AS E ON ET.nombre_equipo = E.nombre
+                    WHERE ET.nombre_torneo = %s;''', (id_torneo,))
+        datos_inscritos = cur.fetchall()
+        cur.execute('''
+                    SELECT ST.nombre_sponsor, ST.monto AS Monto_aportado, S.industria
+                    FROM Sponsor_del_torneo AS ST
+                    JOIN Sponsor AS S ON ST.nombre_sponsor = S.nombre
+                    WHERE ST.nombre_torneo = %s;''',(id_torneo,))
+        datos_sponsors = cur.fetchall()
+        
+    conn.close()
+    
+    return render_template('basicos.html', partidas=datos_partidas,inscritos=datos_inscritos,sponsors = datos_sponsors)
 @app.route('/stats/<string:id_torneo>')
 def stats(id_torneo):
     conn = get_db_connection()
     with conn.cursor() as cur:
-        cur.execute('SELECT * FROM test WHERE columna2= %s;', (id_torneo,))
-        datos_especificos = cur.fetchall()
+        cur.execute('''
+                    SELECT P."id", P.nombre_equipo1, P.puntaje_equipo1, P.nombre_equipo2, P.puntaje_equipo2, P.inicio
+                    FROM Partida AS P 
+                    WHERE P.nombre_torneo = %s; ''', (id_torneo,))
+        datos_partidas = cur.fetchall()
+        cur.execute('''
+                    SELECT E.nombre AS Equipos_Inscritos, E.capitan AS Capitan, E.fecha_creacion
+                    FROM Esta_en_torneo AS ET
+                    JOIN Equipo AS E ON ET.nombre_equipo = E.nombre
+                    WHERE ET.nombre_torneo = %s;''', (id_torneo,))
+        datos_inscritos = cur.fetchall()
+        cur.execute('''
+                    SELECT ST.nombre_sponsor, ST.monto AS Monto_aportado, S.industria
+                    FROM Sponsor_del_torneo AS ST
+                    JOIN Sponsor AS S ON ST.nombre_sponsor = S.nombre
+                    WHERE ST.nombre_torneo = %s;''',(id_torneo,))
+        datos_sponsors = cur.fetchall()
+        
     conn.close()
     
-    return render_template('stats.html', info=datos_especificos)
+    return render_template('basicos.html', partidas=datos_partidas,inscritos=datos_inscritos,sponsors = datos_sponsors)
 
-@app.route('/inscripcion')
+@app.route('/inscripcion',methods=['GET','POST'])
 def inscribirse():
-    return render_template('inscribete.html')
+    if request.method == 'GET':
+        return render_template('inscribete.html')
 
+    torneo = request.form.get('tournament')
+    equipo = request.form.get('team_name')
+        
+    conn = get_db_connection()
+    with conn:
+        with conn.cursor() as cur:
+            if request.method ==  'POST':
+                cur.execute('SELECT max_equipos, fecha_inicio FROM torneo WHERE nombre = %s', (torneo,))
+                maxi = cur.fetchone()
+                
+                if maxi == None:
+                    mensaje = "El torneo indicado no existe"
+                else:
+                    fecha_torneo = maxi['fecha_inicio']
+                    cur.execute('SELECT 1 FROM esta_en_torneo WHERE nombre_equipo = %s AND nombre_torneo = %s;',(equipo,torneo))
+                    inscrito = cur.fetchone()
+                    if inscrito:
+                        mensaje = f"El equipo '{equipo}' ya se encuentra inscrito en el torneo por lo que no es posible incribirlo de nuevo '{torneo}'."
+                    else:   
+                        capacidad = maxi['max_equipos']
+                        cur.execute('SELECT COUNT(*) AS total_inscritos FROM esta_en_torneo WHERE nombre_torneo = %s;', (torneo,))
+                        resultado = cur.fetchone()
+                        inscritos_actuales = resultado['total_inscritos']
+                        if inscritos_actuales >= capacidad:
+                            mensaje = f"¡Lo sentimos! El torneo '{torneo}' ya alcanzó su límite de {capacidad} equipos."
+                        else:
+                            cur.execute('INSERT INTO esta_en_torneo VALUES (%s, %s,%s)',(equipo,torneo,fecha_torneo))
+                            mensaje = "¡Inscripcion realizada con exito!"
+    conn.close()
+        
+    return render_template('inscribete.html',mensaje=mensaje)
+    
 @app.route('/videojuegos')
 def videojuegos():
     conn = get_db_connection()
@@ -78,15 +168,29 @@ def videojuegos():
     
     return render_template('videojuegos.html', juegos=juego)
 
-@app.route('/videojuegos/<string:id_juego>')
+@app.route('/sponsors/<string:id_juego>')
 def sponsor(id_juego):
     conn = get_db_connection()
     with conn.cursor() as cur:
-        cur.execute('SELECT * FROM test WHERE columna2= %s;', (id_juego,))
-        datos_especificos = cur.fetchall()
+        cur.execute('''
+                    SELECT S2.NOMBRE, S2.INDUSTRIA, SUM(ST2.MONTO) AS monto
+                    FROM SPONSOR_DEL_TORNEO AS ST2 JOIN (
+                        SELECT S.NOMBRE, S.INDUSTRIA
+                        FROM SPONSOR AS S
+                        WHERE NOT EXISTS (
+                            SELECT 1
+                            FROM TORNEO AS TN
+                            WHERE TN.VIDEOJUEGO = %s AND NOT EXISTS (
+                                 SELECT 1
+                                FROM SPONSOR_DEL_TORNEO AS ST
+                                WHERE ST.NOMBRE_TORNEO = TN.NOMBRE AND ST.NOMBRE_SPONSOR = S.NOMBRE
+                            )
+                        )) AS S2 ON ST2.NOMBRE_SPONSOR = S2.NOMBRE 
+                    GROUP BY S2.NOMBRE, S2.INDUSTRIA;''', (id_juego,))
+        datos_db = cur.fetchall()
     conn.close()
     #falta sponsorhtml y la consulta
-    return render_template('sponsors.html', info=datos_especificos)
+    return render_template('sponsors.html', info=datos_db)
     
 
 
