7fbfe017995aee4441c69a81ec032c216e10211a
CONSTANZA VENEGAS <constanza.venegas@estudiante.uc.cl>
2026-04-13 20:51:50 -0400

    consulta torneo terminada

--- T1/bdd/consultas/consultas_torneos.sql
+++ T1/bdd/consultas/consultas_torneos.sql
@@ -4,7 +4,48 @@ FROM Torneo;
 
 
 -- 1.1. Tabla de posiciones de la fase de grupos
--- ME FALTA ESTA CONSULTA, DUDA LA MANDÉ A WHATSAPP JIJI
+-- Esta tabla contiene la Fase A/B, #partidas jugadas, #wins, #empates, #derrotas, puntaje_total
+-- Quedan juntos por fase, y por grupo
+
+SELECT
+    ROW_NUMBER() OVER(PARTITION BY fase ORDER BY puntaje_total DESC) AS ranking,
+    fase,
+    equipo,
+    partidas_jugadas,
+    ganadas,
+    empatadas,
+    perdidas,
+    puntaje_total
+FROM(
+    SELECT 
+        fase, 
+        equipo,
+        COUNT(*) AS partidas_jugadas,
+        SUM(CASE WHEN puntos_partida = 3 THEN 1 ELSE 0 END) AS ganadas,
+        SUM(CASE WHEN puntos_partida = 1 THEN 1 ELSE 0 END) AS empates,
+        SUM(CASE WHEN puntos_partida = 0 THEN 1 ELSE 0 END) AS perdidas,
+        SUM(puntos_partida) AS puntaje_total
+    FROM(
+        -- Vista como equipo 1:
+        SELECT 
+            fase, 
+            nombre_equipo1 AS equipo, 
+            puntaje_equipo1 AS puntos_partida,
+
+        FROM Partida
+        WHERE nombre_torneo = %s AND fase IN ('Grupo A', 'Grupo B')
+
+        UNION ALL
+
+        -- Vista como equipo 2:
+        SELECT 
+            fase, 
+            nombre_equipo2 AS equipo, 
+            puntaje_equipo2 AS puntos_partida
+        FROM Partida
+        WHERE nombre_torneo = %s AND fase IN ('Grupo A', 'Grupo B')) as Resultados
+    GROUP BY fase, equipo) AS Tabla_intermedia
+    ORDER BY fase, ranking;
 
 -- 1.2 Partidas (del torneo) jugadas con sus resultados
 SELECT P."id", P.nombre_equipo1, P.puntaje_obtenido1, P.nombre_equipo2, P.puntaje_obtenido2, P.inicio
